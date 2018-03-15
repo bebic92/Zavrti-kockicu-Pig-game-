a2e6d3989a9059e7e84d002f59b3c83a5f821412
Marin Bebic <=marin.bebic@gmail.com>
2018-03-15 19:27:12 +0100

    Kreiran izgled aplikacije(css i html)

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Pravila Igre:
+
+- Igra ima 2 igraca, igra se u rundama
+- U svakoj rundi, igrac baca kockicu koliko god puta zeli. Svaki broj dodaje se trenutnom zbroju 
+- Ali, ako se kockica okrene na broj 1, trenutni zbroj se gubi. Nakon toga, igra slijedeci igrac
+- Međutim klikom na botun 'Spremi', trenutni zbroj se dodaje ukupnom zbroju . Nakon toga igra slijedeci igrac
+- Prvi igrac koji dode do 100 bodova u ukupnom zbroju pobjeduje
+
+*/
